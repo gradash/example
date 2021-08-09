d2f39f1627601bab032ec956d57c0e81a7464f7e
matvejevs <matvejevs@MATVEJEVS-W>
2021-08-09 12:44:12 +0300

    lesson 2

---
+++ lesson4.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+    $x=10;
+    $y=20;
+
+    echo $x + $y.'</br>';
+    echo $x - $y.'</br>';
+    echo $x * $y.'</br>';
+    echo $x / $y.'</br>';
+    echo $x % $y.'</br>';
+
+    $x += 10; // $x = $x + 10;
+    echo $x.'</br>';
+
+    echo M_PI.'</br>';
+    echo M_E.'</br>';
+
+
+
+
+
+
+?>
+
+</body>
+</html>
