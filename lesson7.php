dca5e8a6fa7e52aa0125229d7d0be2e28879f566
matvejevs <matvejevs@MATVEJEVS-W>
2021-08-09 16:34:15 +0300

    lesson 7

---
+++ lesson7.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+
+$x = 99;
+
+switch ($x){
+    case 6:
+        echo "Var: 6";
+        break;
+    case 7:
+        echo "Var: 7";
+        break;
+    case 8:
+        echo "Var: 8";
+        break;
+    case 9:
+        echo "Var: 9";
+        break;
+    default:
+        echo "default value";
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+</body>
+</html>
