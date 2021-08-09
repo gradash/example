def2c3b5a23bf48ad0a07b38a699c9bbf22b4424
matvejevs <matvejevs@MATVEJEVS-W>
2021-08-09 10:33:21 +0300

    lesson 2

--- index.php
+++ index.php
@@ -9,12 +9,28 @@
 </head>
 <body>
 <?php
-    echo "<b>PHP</b> Storm";
+ /*   echo "<b>PHP</b> Storm";
     echo "<br>Learning";
     echo "<br>\"Brackets\"";
-    echo '<br>"Brackets2"'
+    echo '<br>"Brackets2"';
+*/
 
 
+    const MY_AGE = 37;
+    echo "MY_AGE Const: ".MY_AGE.'<br>';
+    $number = 42; //int
+    $num = 0.55; // float
+
+    $str = "Variable: "; //string
+    $bool = true; //bool
+
+    $a = 0.5; //float
+    $b = "0.5"; //string
+    echo $a + floatval($b); // type conversion
+
+
+
+    echo $str.$number.'. '.'Variable2: '.$num.'.';
 
 
 
